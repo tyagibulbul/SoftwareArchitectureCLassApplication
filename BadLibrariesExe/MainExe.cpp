35761ae961400ed7a8bfe4ff965c9d2807fc1934
olekaspt <olekaspt@mail.uc.edu>
2022-04-04 22:02:58 -0400

    Upate to use Gtest

--- BadLibrariesExe/MainExe.cpp
+++ BadLibrariesExe/MainExe.cpp
@@ -1,7 +1,6 @@
 // BadLibrariesExe.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#include "..\Core\LinkedList.h"
 #include <iostream>
 #include <string>
 #include <cmath>
