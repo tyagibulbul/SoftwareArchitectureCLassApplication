e2b432f075a404402e345663c1b15a7ab6c23210
tyagibulbul <123034704+tyagibulbul@users.noreply.github.com>
2023-04-08 03:03:29 -0400

    Update Program.cs

--- DotNetAutomationProgram/Program.cs
+++ DotNetAutomationProgram/Program.cs
@@ -1,11 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
+// See https://aka.ms/new-console-template for more information
 using DotNetAutomationBinding;
+using System;
 
-Console.WriteLine("Hello, World!");
+namespace DotNetAutomationConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World from DotNetAutomationConsoleApp!");
 
+            Session theSession = Session.GetSession;
+            
+            Part thePart = theSession.MakePart("SomePart.prt");
 
-Session theSession = Session.GetSession;
-
-Part thePart = theSession.MakePart("SomePart.prt");
-
-thePart.Save();
+            thePart.Save();
+            Console.WriteLine("Part saved");
+        }
+    }
+}
